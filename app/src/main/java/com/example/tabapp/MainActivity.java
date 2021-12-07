0bf9e0a32968883734313af215e5674c3cad39b8
Yash Kumar <kumaryash7111@gmail.com>
2021-12-07 15:29:41 +0530

    My initial commit

---
+++ app/src/main/java/com/example/tabapp/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.tabapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TabLayout tabLayout = findViewById(R.id.tabBar);
+        TabItem tabChats = findViewById(R.id.tabChats);
+        TabItem tabStatus = findViewById(R.id.tabStatus);
+        TabItem tabCalls = findViewById(R.id.tabCalls);
+
+        ViewPager viewPager = findViewById(R.id.viewPager);
+
+        PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
+
+        viewPager.setAdapter(pagerAdapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+    }
+}
